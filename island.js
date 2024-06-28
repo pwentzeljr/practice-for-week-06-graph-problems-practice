ccb93d5a297cd65134c1ae34c3a8a7911d26464a
Paul Wentzel <paul.wentzel@gmail.com>
2024-06-28 13:24:40 -0400

    tests passing

--- island.js
+++ island.js
@@ -11,6 +11,17 @@ function getNeighbors(row, col, graph) {
   // Return neighbors
 
   // Your code here
+  const rv = [];
+
+  if (row > 0 && graph[row - 1][col] === 1) rv.push([row - 1, col]);
+
+  if (row < graph.length - 1 && graph[row + 1][col] === 1) rv.push([row + 1, col]);
+
+  if (col > 0 && graph[row][col - 1] === 1) rv.push([row, col - 1]);
+
+  if (col < graph[0].length - 1 && graph[row][col + 1] === 1) rv.push([row, col + 1]);
+
+  return rv;
 }
 
 
@@ -38,6 +49,25 @@ function islandSize(row, col, graph) {
   // return size
 
   // Your code here
+  if (graph[row][col] === 0) return 0;
+
+  const start = [row, col];
+  const stack = [start];
+  const visited = new Set([start.toString()]);
+
+
+  while (stack.length > 0) {
+    const curr = stack.pop();
+    const neighbors = getNeighbors(curr[0], curr[1], graph);
+
+    for (const neighbor of neighbors) {
+      if (!visited.has(neighbor.toString())) {
+        stack.push(neighbor);
+        visited.add(neighbor.toString());
+      }
+    }
+  }
+  return visited.size;
 }
 
 module.exports = [getNeighbors, islandSize];
